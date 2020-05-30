7fb5000ab73cfecbf5fd046bdf71543d7c29c14b
jacobtdp <jacobsantiagodennis@gmail.com>
2020-05-29 20:26:08 -0700

    sidebar and styling for individual blog post pages

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,9 @@ import Helmet from 'react-helmet'
 import get from 'lodash/get'
 import Img from 'gatsby-image'
 import Layout from '../components/layout'
+import Footer from '../components/footer'
+import Sidebar from '../components/Sidebar'
+import './blog-post.css'
 
 import heroStyles from '../components/hero.module.css'
 
@@ -11,6 +14,7 @@ class BlogPostTemplate extends React.Component {
   render() {
     const post = get(this.props, 'data.contentfulBlogPost')
     const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+    const sidebarStory = get(this, 'props.data.allContentfulSidebarStory.edges')
 
     return (
       <Layout location={this.props.location}>
@@ -18,7 +22,7 @@ class BlogPostTemplate extends React.Component {
         <div className="content-container">
           <div className="left-ad"></div>
 
-          <div style={{ background: '#fff' }}>
+          <div className="blog-post" style={{ background: '#fff' }}>
             <Helmet title={`${post.title} | ${siteTitle}`} />
             <div className={heroStyles.hero}>
               <Img
@@ -43,9 +47,20 @@ class BlogPostTemplate extends React.Component {
               />
             </div>
           </div>
+
+          <ul className="sidebar">
+            {sidebarStory.map(({ node }) => {
+              return (
+                <li key={node.slug}>
+                  <Sidebar sidebarStory={node} sidebarBody={node.body.content[0].content[0].value}/>
+                </li>
+              )
+            })}
+          </ul>
         
         <div className="right-ad"></div>
         </div>
+        <Footer />
 
       </Layout>
     )
@@ -56,6 +71,7 @@ export default BlogPostTemplate
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
+
     contentfulBlogPost(slug: { eq: $slug }) {
       title
       publishDate(formatString: "MMMM Do, YYYY")
@@ -67,6 +83,32 @@ export const pageQuery = graphql`
       body {
         childMarkdownRemark {
           html
+        }
+      }
+    }
+    allContentfulSidebarStory(sort: {order: DESC}) {
+      edges {
+        node {
+          author {
+            name
+          }
+          body {
+            body
+            content {
+              content {
+                value
+              }
+            }
+          }
+          publishDate(formatString: "MMMM Do YYYY")
+          slug
+          subject
+          title
+          heroImage {
+            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
         }
       }
     }
